e64adab64164512258b921f67922cc0ec34bcdf7
Ashwin Murali <ashwin.murali@gmail.com>
2016-04-11 13:32:01 +0530

    testing changes add

---
+++ test/com/cruisemaniac/HelloServletTest.java
@@ -0,0 +1,20 @@
+package com.cruisemaniac;
+
+import junit.framework.Assert;
+import org.junit.Test;
+
+public class HelloServletTest {
+	
+	//sample test case
+	@Test
+	public void testPrintHello() {
+		Assert.assertEquals(true, true);
+	}
+	
+	
+	@Test
+	public void testFailPrintHello() {
+		Assert.assertEquals(false, true);
+	}
+	
+}
